a2446c575bfb68e404e6c3d91b24a64fd423a1ef
Jathmel Gordon <jathmel@gmail.com>
2018-05-03 22:19:50 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, value|
     if prev_value == 0
       prev_value = value
+      key_arr << name
     else
       if prev_value > value 
         key_arr.unshift(name)
